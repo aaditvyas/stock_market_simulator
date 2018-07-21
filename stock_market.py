13942b68cbd29e66c1738b799743d9bc703a27f2
Aadit Vyas <aadit.vyas@yale.edu>
2018-07-21 15:15:57 -0400

    Formatted Print and Fixed Sorting
    Reversed Boolean representation of "visibility" so that sort() worked as expected: moving the hidden bids below the visible ones at the same price.

--- stock_market.py
+++ stock_market.py
@@ -1,14 +1,14 @@
 import datetime
 
 class Bid:
-	def __init__(self, price, visible, trader):
+	def __init__(self, price, visibility, trader):
 		self.price = price
-		self.visible = visible
+		self.visibility = visibility # True = visible, False = Hidden
 		self.time = None  # not yet submitted
 		self.trader = trader
 
 	def __str__(self):  # allows us to call str(my_bid) and print(my_bid)
-		return "{}\t{}\t{}\t{}".format(bid.price, bid.visibility, bid.time, bid.trader)
+		return "{}\t{}\t{}\t{}".format(self.price, self.visibility, self.time.strftime("%H:%M:%S:%f"), self.trader)
 
 class Book:
 	def __init__(self):
@@ -30,21 +30,21 @@ class Book:
 
 def main():
 	# Bids
-	florio_bid = Bid(20.06, True, "Florio")
-	amy_bid = Bid(20.05, True, "Amy")
-	dmitri_bid = Bid(20.05, False, "Dmitri")
-	brian_bid = Bid(20.04, False, "Brian")
-	chao_bid = Bid(20.04, False, "Chao")
-	esteban_bid = Bid(20.03, False, "Esteban")
+	amy_bid = Bid(20.05, False, "Amy")
+	brian_bid = Bid(20.04, True, "Brian")
+	chao_bid = Bid(20.04, True, "Chao")
+	dmitri_bid = Bid(20.05, True, "Dmitri")
+	esteban_bid = Bid(20.03, True, "Esteban")
+	florio_bid = Bid(20.06, False, "Florio")
 
 	# Bid book
 	book = Book()
-	book.submit_bid(florio_bid)
-	book.sumbit_bid(amy_bid)
-	book.submit_bid(dmitri_bid)
+	book.submit_bid(amy_bid)
 	book.submit_bid(brian_bid)
 	book.submit_bid(chao_bid)
+	book.submit_bid(dmitri_bid)
 	book.submit_bid(esteban_bid)
+	book.submit_bid(florio_bid)
 
 	# Testing
 	print(book)
